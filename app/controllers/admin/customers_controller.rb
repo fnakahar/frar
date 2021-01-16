937626509e0af507b0fa8d2aa597b2359530da09
Futoshi Nakahara <fnakahar@gmail.com>
2021-01-16 23:40:43 +0900

    Customer CRUD implementation finished

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,12 +4,15 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def index
+    @customer = Customer.all
   end
 
   def create
@@ -21,11 +24,27 @@ class Admin::CustomersController < ApplicationController
     end
   end
 
+  def update
+    @customer = Customer.find(params[:id])
+
+    if @customer.update(customer_params)
+      redirect_to admin_customer_url(@customer), notice: "「#{@customer.name}」様の登録情報を変更いたしました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to admin_customer_url, notice: "「#{@customer.name}」様の登録情報を削除いたしました。"
+  end
+
 
   private
 
   def customer_params
-    params.require(:customer).permit(:name, :age_range, :gender, :email, :password, :password_confirmation)
+    params.require(:customer).permit(:name, :age_range, :gender, :email, :password, :password_confirmation, :admin)
   end
 
 end
