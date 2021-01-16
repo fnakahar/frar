99559ca75203d0ee6a5ae110db9e1b8ff7263afc
Futoshi Nakahara <fnakahar@gmail.com>
2021-01-17 01:34:42 +0900

    Customer Login implementation finished

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    customer = Customer.find_by(email: session_params[:email])
+
+    if customer&.authenticate(session_params[:password])
+      session[:customer_id] = customer.id
+      redirect_to root_url, notice: 'ログインしました。'
+    else
+      flash.now[:alert] = "ログインに失敗しました"
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+  get   '/login', to: 'sessions#new'
+  post  '/login', to: 'sessions#create'
+
   namespace :admin do
     resources :customers
   end
+
   root 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :product_admins
   resources :product_users, only: [:show, :index]
