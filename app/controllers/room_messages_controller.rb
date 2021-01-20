e49f8d12ae0eed775c19e210d8a70257d10d5318
Futoshi Nakahara <fnakahar@gmail.com>
2021-01-20 22:20:32 +0900

    Chat | Before start Websocket-Actioncable implementation

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -1,2 +1,15 @@
 class RoomMessagesController < ApplicationController
+  before_action :load_entities
+
+  def create
+    @room_message = RoomMessage.create customer: current_customer,
+                                       room: @room,
+                                       message: params.dig(:room_message, :message)
+  end
+
+  protected
+
+  def load_entities
+    @room = Room.find params.dig(:room_message, :room_id)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravatar_url(customer)
+    gravatar_id = Digest::MD5::hexdigest(customer.email).downcase
+    url = "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
